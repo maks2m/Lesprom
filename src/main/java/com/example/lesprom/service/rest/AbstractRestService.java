88bf1a7759aabe905f5687f1d67b93199f0ec2d4
melkin <melkin@egartech.ru>
2022-09-06 10:11:30 +0300

    add comments in service interface and abstract class

--- src/main/java/com/example/lesprom/service/rest/AbstractRestService.java
+++ src/main/java/com/example/lesprom/service/rest/AbstractRestService.java
@@ -2,6 +2,11 @@ package com.example.lesprom.service.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * Абстрактный сервисный класс, реализующий интерфейс стандартного рест сервиса
+ * @param <E> дженерик сущности, для которой будет реализовываться интерфейс
+ * @param <R> дженерик репозитория сущности, для которой будет реализовываться интерфейс
+ */
 public abstract class AbstractRestService<E, R> implements RestService<E> {
 
     protected final R repository;
